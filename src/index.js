f42ed77ef1a5efb7275f9c496fbab46352378a9d
Anton <ant.roma24@gmail.com>
2021-02-11 19:46:42 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (array != undefined && array.length > 1) {
+    return array.sort((a, b) => a - b)[0];
+  } else {
+    return 0;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array != undefined && array.length > 1) {
+    return array.sort((a, b) => a - b)[array.length - 1];
+  } else {
+    return 0;
+  }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array != undefined && array.length > 1) {
+    return array.reduce((acc, i) => acc + i) / array.length;
+  } else {
+    return 0;
+  }
+};
